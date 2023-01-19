0ce7b3f4d2a4c3febd9f947686d0c0924a484e2b
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2023-01-19 21:24:47 +0700

    เพิ่มการดึง public key

--- server-ts/src/auth.ts
+++ server-ts/src/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import appConfig from "config"
+import appConfig from "./config"
 import jwt from 'jsonwebtoken'
 import Koa from 'koa'
 
@@ -16,6 +16,18 @@ export async function  initSsoCert() {
 }
 
 export async function verifyJwt(token: string){
+    if(appConfig.isDev && token.startsWith('DEV::')){
+        const items = token.split('::')
+        const data =  {
+          preferred_username: items[1],
+          groups: ['student']
+        }
+        if(items.length >= 3 && items[2] == '1'){
+          data.groups.push('staff')
+        }
+        return data
+      }
+
     return new Promise<any>((resolve, reject) => {
         jwt.verify(token, public_key as jwt.Secret, { issuer: appConfig.ssoIssuer, algorithms: ['RS256'] },(err,decoded) => {
             if(err){
@@ -34,7 +46,7 @@ export async function authMiddleware(ctx: Koa.Context, next: () => Promise<any>)
         const token = bearer.substring(BEARER_PREFIX.length)
         const decoded = await verifyJwt(token)
         const authData: AuthData = {
-            username: decoded.prefered_username as string,
+            username: decoded.preferred_username as string,
             isStaff: (decoded.groups as string[])?.indexOf('staff') >= 0
         }
         ctx.state.authData = authData
--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,17 +1,22 @@
 import Koa from 'koa'
-import json from 'koa-json'
 import apiRouter from './api'
 import loadFixtures from './fixture'
 import appConfig from './config'
 import { koaBody } from 'koa-body'
+import { initSsoCert } from './auth'
+import cors from '@koa/cors'
+
 
 
 const app = new Koa()
 
-app.use(json())
-app.use(koaBody())
-app.use(apiRouter.routes())
+app.use(cors());
+app.use(koaBody());
+app.use(apiRouter.routes());
 
-app.listen(8000)
-
-loadFixtures(appConfig.ClearDataBeforeLoad)
+(async () => {
+    await loadFixtures(appConfig.ClearDataBeforeLoad)
+    await initSsoCert()
+    app.listen(8000)
+    console.log('Server is ready at port 8000')
+})();
