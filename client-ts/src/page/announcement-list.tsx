29168057783a167dc307df536e0be87e4a7fdc8b
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2023-01-23 14:33:13 +0700

    เพิ่ม feature create announcement

--- client-ts/src/page/announcement-list.tsx
+++ client-ts/src/page/announcement-list.tsx
@@ -1,14 +1,19 @@
 import { useState, useEffect, ChangeEvent } from "react";
-import {Grid, TextField, Typography } from "@mui/material";
+import { Button,Dialog,DialogTitle,Grid, IconButton, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import AnnouncementCard from "../component/announcement-card";
 import PannAppBar from "../component/pann-app-bar";
 import Announcement from "../models/Announcement";
-import Repo from '../repositories'
+import Repo from '../repositories';
+import { Add, Close} from '@mui/icons-material';
+import { useAuth } from "react-oidc-context";
+import AnnouncementForm from "../component/announcement-form";
 
 function AnnouncementList() {
+    const auth = useAuth()
     const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
     const [searchFilter, setSearchFilter] = useState('')
+    const [createFormPopup, setCreateFormPopup] = useState(false)
 
     const fetchAnnouncementList = async () => {
         let params = {
@@ -21,10 +26,18 @@ function AnnouncementList() {
             }
             setAnnouncementList(result)
         }
+        
     }
 
     const handleChangeSearchFilter = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchFilter(event.target.value)
+    }
+
+    const onCreateAnnouncement = async (ann: Partial<Announcement>) => {
+        ann.userCode = auth.user?.profile.preferred_username
+        await Repo.announcements.create(ann)
+        fetchAnnouncementList()
+        setCreateFormPopup(false)
     }
 
     useEffect(() => {
@@ -35,6 +48,9 @@ function AnnouncementList() {
         <Box sx={{ flexGrow: 1 }}>
             <PannAppBar></PannAppBar>
             <TextField sx={{ m: 2, minWidth: 120}} label='Search' placeholder="Topic, Description" variant="outlined" value={searchFilter} onChange={handleChangeSearchFilter} />
+            <Button sx={{ m: 2, float: 'right' }} variant="contained" onClick={() => setCreateFormPopup(true)}>
+                <Add/> Announcement
+            </Button>
             {announcementList.length
               ?
               <Grid container sx={{ p: 2}} spacing={{ xs:2, md: 3}} columns={{ xs: 2, sm: 8, md: 12, lg:12, xl:10}}>
@@ -49,6 +65,15 @@ function AnnouncementList() {
                 <Typography variant="body2" color='text.secondary'>No Announcement Found</Typography>
               </Box>
             }
+            <Dialog PaperProps={{ sx: { minWidth: '50%' } }} open={createFormPopup} onClose={() => setCreateFormPopup(false)}>
+                <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between'}}>
+                    Create Announcement
+                    <IconButton onClick={() => setCreateFormPopup(false)}>
+                        <Close/>
+                    </IconButton>
+                </DialogTitle>
+                <AnnouncementForm announcement={{}} callbackFn={onCreateAnnouncement}></AnnouncementForm>
+            </Dialog>
         </Box>
     )
 }
