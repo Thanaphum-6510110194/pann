8992db794e0694053f9bb6755c12301d7f3758ad
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2023-01-23 15:45:29 +0700

    ปรับการ redirect หาก login ด้วย staff user

--- client-ts/src/page/login.tsx
+++ client-ts/src/page/login.tsx
@@ -42,6 +42,11 @@ function Login() {
     if (auth.isAuthenticated) {
         if (userInfo.ready) {
             const backTo = location.state?.bactTo || '/home'
+            if(action.isStaff()){
+                return (
+                    <Navigate to='/announcement' replace />
+                )
+            }
             return (
                 <Navigate to={backTo} replace />
             )
