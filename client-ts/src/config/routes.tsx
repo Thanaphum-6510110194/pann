1a1be80d4e6577d37d56957df2db3cbb9030e488
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2023-01-23 14:10:02 +0700

    เพิ่ม routes ไปยัง announcement page

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -2,6 +2,7 @@ import {Route, Routes, useLocation, Navigate} from "react-router-dom"
 import { useAppCtx } from "../AppProvider"
 import Login from "../page/login"
 import UserResultList from "../page/user-result-list"
+import AnnouncementList from "../page/announcement-list"
 
 type Props = {
     staffOnly?: boolean
@@ -29,6 +30,7 @@ const AppRoutes = () => {
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
             <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
+            <Route path='announcement' element={<ProtectedRoute><AnnouncementList/></ProtectedRoute>}/>
         </Routes>
     )
 }
